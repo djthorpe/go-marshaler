a4ea76fcf29a289870e41cdac4fa489a99750551
David Thorpe <djt@mutablelogic.com>
2021-08-27 10:29:36 +0200

    Updated decoder tests

--- decoder_test.go
+++ decoder_test.go
@@ -8,7 +8,8 @@ import (
 )
 
 type ts struct {
-	Time time.Time `yaml:"timestamp"`
+	Time     time.Time     `yaml:"timestamp"`
+	Duration time.Duration `yaml:"duration"`
 }
 
 func Test_Decoder_001(t *testing.T) {
@@ -43,11 +44,31 @@ func Test_Decoder_003(t *testing.T) {
 func Test_Decoder_004(t *testing.T) {
 	dest := ts{}
 	src := map[string]interface{}{
-		"timestamp": "test",
+		"timestamp": "2006",
 	}
-	if err := marshaler.NewDecoder("yaml", marshaler.ConvertTime).Decode(src, &dest); err != nil {
-		t.Fatal(err)
-	} else if dest.Time.IsZero() == false {
-		t.Fatal("Unexpected non-zero time")
+	if err := marshaler.NewDecoder("yaml", marshaler.ConvertTime).Decode(src, &dest); err == nil {
+		t.Fatal("Expected error")
+	}
+}
+
+func Test_Decoder_005(t *testing.T) {
+	dest := ts{}
+	src := map[string]interface{}{}
+	tests := []struct {
+		src  interface{}
+		dest time.Duration
+	}{
+		{"100ms", 100 * time.Millisecond},
+		{"10", 10 * time.Second},
+		{"50h", 50 * time.Hour},
+		{int(314), 314 * time.Second},
+	}
+	for _, test := range tests {
+		src["duration"] = test.src
+		if err := marshaler.NewDecoder("yaml", marshaler.ConvertTime, marshaler.ConvertDuration).Decode(src, &dest); err != nil {
+			t.Fatal(err)
+		} else if dest.Duration != test.dest {
+			t.Fatal("Unexpected value", dest.Duration, " expected ", test.dest)
+		}
 	}
 }
